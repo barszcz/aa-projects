003157df391b3b56f42b5f2ad54df2ace66c2d9c
Jake Barszcz and Michael Doliner <jake+michael@appacademy.io>
2014-11-21 10:27:49 -0500

    Creates my_uniq.

--- array-tdd/spec/remove_dups_spec.rb
+++ array-tdd/spec/remove_dups_spec.rb
@@ -8,13 +8,13 @@ describe 'Array#my_uniq' do
   end
 
   it 'returns a duplicate-free array untouched' do
-    expect([1,2,3,4,5].my_uniq).to be([1,2,3,4,5])
+    expect([1,2,3,4,5].my_uniq).to eq([1,2,3,4,5])
   end
 
   it 'removes duplicates from an array' do
     test_arr = [1,3,5,3,4,3,5,2]
     (1..5).each do |num|
-      expect(test_arr.my_uniq.count(num)).to be(1)
+      expect(test_arr.my_uniq.count(num)).to eq(1)
     end
     # expect(test_arr.my_uniq.count(1)).to be(1)
     # expect(test_arr.my_uniq.count(2)).to be(1)
@@ -25,7 +25,7 @@ describe 'Array#my_uniq' do
 
   it 'returns unique elements in order of their first appearance' do
     test_arr = [1,3,5,3,4,3,5,2]
-    expect(test_arr.my_uniq).to be([1,3,5,4,2])
+    expect(test_arr.my_uniq).to eq([1,3,5,4,2])
   end
 
 end
