b4048432e3ab776c344eeb2e728900f90c1bf5e4
Jake + Justin <jake+justin@appacademy.io>
2014-12-01 16:34:15 -0500

    Bonuses

--- app/models/response.rb
+++ app/models/response.rb
@@ -15,10 +15,14 @@ class Response < ActiveRecord::Base
   has_one :question, through: :answer_choice, source: :question
 
   def sibling_responses
-    self.question.responses.where("responses.id != ?", self.id)
+    # self.question.responses.where("responses.id != ?", self.id)
+    Response.joins(:answer_choice).distinct
+      .joins("JOIN questions ON answer_choices.question_id = questions.id")
+      .joins("JOIN answer_choices AS all_answers ON questions.id = all_answers.question_id")
+      .where("answer_choices.id = #{self.answer_choice_id} AND responses.id != #{self.id}")
   end
 
-#  private
+  private
   def respondent_has_not_already_answered_question
     if sibling_responses.exists?(user_id: self.user_id)
       errors[:user_id] << "this user already responded to this question"
@@ -26,8 +30,11 @@ class Response < ActiveRecord::Base
   end
 
   def author_cannot_respond_to_own_poll
-
-    if question.poll.author_id == self.user_id
+    query = Poll.joins(:questions)
+      .joins("JOIN answer_choices ON questions.id = answer_choices.question_id")
+      .where("answer_choices.id = #{self.answer_choice_id}")
+      .select("polls.author_id")
+    if query.first.author_id == self.user_id
       errors[:user_id] << "A poll's author cannot respond to their own poll"
     end
   end
