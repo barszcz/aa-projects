d9b189b3f26971571655f8cd08d3d7e6b72881fe
Jake and Anthony <jake+anthony@appacademy.io>
2014-12-04 18:04:24 -0500

    Part I

---
+++ app/controllers/cats_controller.rb
@@ -0,0 +1,47 @@
+class CatsController < ApplicationController
+  def index
+    @cats = Cat.all
+    render :index
+  end
+
+  def new
+    @cat = Cat.new
+    render :new
+  end
+
+  def show
+    @cat = Cat.find(params[:id])
+    render :show
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      render :show
+    else
+      render json: @cat.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+
+    if @cat.update(cat_params)
+      render :show
+    else
+      render json: @cat.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @cat = Cat.find(params[:id])
+
+    render :edit
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :color, :birth_date, :sex, :description)
+  end
+end
---
+++ app/models/cat_rental_request.rb
@@ -0,0 +1,50 @@
+class CatRentalRequest < ActiveRecord::Base
+  validates :cat_id, :start_date, :end_date, :status, presence: true
+  validates :status, inclusion: { in: %w(PENDING APPROVED DENIED) }
+  validate :approved_requests_cannot_overlap
+
+  after_initialize { self.status ||= "PENDING" }
+
+  belongs_to(
+    :cat,
+    class_name: "Cat",
+    foreign_key: :cat_id,
+    primary_key: :id
+  )
+
+  def overlapping_requests
+    self.cat.cat_rental_requests.where(<<-SQL, self.end_date, self.start_date, self.id, self.id)
+      NOT (start_date > ? OR end_date < ? )
+      AND (? IS NULL OR id != ?)
+    SQL
+  end
+
+  def approved_requests_cannot_overlap
+    if overlapping_approved_requests.exists? && self.status != "DENIED"
+      errors[:base] << "Request overlaps with another rental"
+    end
+  end
+
+  def overlapping_approved_requests
+    overlapping_requests.where(status: "APPROVED")
+  end
+
+  def approve!
+    CatRentalRequest.transaction do
+      self.status = "APPROVED"
+      self.save!
+      overlapping_requests.each do |req|
+        req.deny!
+      end
+    end
+  end
+
+  def deny!
+    self.status = "DENIED"
+    self.save!
+  end
+
+  def pending?
+    self.status == "PENDING"
+  end
+end
