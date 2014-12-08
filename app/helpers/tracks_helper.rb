56f6429ca25edddae2dda69570aedbdc9e8083d9
Jake Barszcz <jfbarszcz@gmail.com>
2014-12-08 16:28:51 -0500

    Phase III complete

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,9 @@
 module TracksHelper
+  def ugly_lyrics(lyrics)
+    ugly_lyrics = lyrics.lines
+                        .map { |line| line.prepend("&#9835; ") }
+                        .join("\n")
+
+    "<pre>#{ugly_lyrics}</pre>".html_safe
+  end
 end
