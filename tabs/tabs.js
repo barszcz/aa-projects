42956576c8088c442ea1faea5adf9217580fbace
Jake and Cihangir <jake+cihangir@appacademy.io>
2014-12-18 11:22:36 -0500

    Transitions work properly (w/o effects)

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -5,8 +5,20 @@
   }
 
   $.Tabs = function (el) {
-
+    this.$el = $(el);
+    // this.$contentTabs = $(this.$el.data("content-tabs"));
+    this.$activeTab = $(".active");
+    this.$el.on("click", "a", this.clickTab.bind(this));
   }
+
+  $.Tabs.prototype.clickTab = function (event) {
+    event.preventDefault();
+    $target = $(event.currentTarget);
+    this.$activeTab.removeClass("active");
+    $target.addClass("active");
+    $($target.attr("href")).addClass("active");
+    this.$activeTab = $(".active");
+  };
 
   $.fn.tabs = function () {
     return this.each(function () {
@@ -14,5 +26,5 @@
     });
   };
 
-  
+
 }());
