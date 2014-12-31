4d06fe21187bc2183f46062736ca9f806d615bca
Jeffrey Barszcz <jfbarszcz@gmail.com>
2014-12-31 08:53:04 -0500

    Initial commit

---
+++ app/assets/javascripts/trello_clone.js
@@ -0,0 +1,14 @@
+window.TrelloClone = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert("yo");
+
+  }
+};
+
+$(function () {
+  window.TrelloClone.initialize();
+});
